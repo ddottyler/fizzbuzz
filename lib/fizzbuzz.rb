f22a64ee81f33c814a5a7f102e37f650cf7499b2
Dan T <dp.tyler@hotmail.co.uk>
2020-12-15 16:28:34 +0000

    Fizzbuzz test set up complete

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,12 +1,10 @@
 def fizzbuzz(number)
-  if number == 15
+  if number % 3 == 0 && number % 5 == 0
       "fizzbuzz"
-  elsif number == 5
+  elsif number % 5 == 0
     "buzz"
-  elsif number == 3
+  elsif number % 3 == 0
     "fizz"
-  elsif !number.is_a?(Integer)
-    "Try again"
   else
     number
   end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,24 +1,21 @@
 require './lib/fizzbuzz'
 
 describe 'fizzbuzz' do
-  it 'returns "fizz" when passed 3' do
+
+  it 'returns "fizzbuzz" when number % 3 == 0 && number % 5 == 0' do
+    expect(fizzbuzz(15)).to eq 'fizzbuzz'
+  end
+
+  it 'returns "fizz" when number % 3 == 0' do
     expect(fizzbuzz(3)).to eq 'fizz'
   end
 
-  it 'returns "buzz" when passed 5' do
+  it 'returns "buzz" when number % 5 == 0' do
     expect(fizzbuzz(5)).to eq 'buzz'
-  end
-
-  it 'returns "fizzbuzz" when passed 15' do
-    expect(fizzbuzz(15)).to eq 'fizzbuzz'
   end
 
   it 'returns 7 when passed 7' do
     expect(fizzbuzz(7)).to eq 7
-  end
-
-  it 'returns "Try again" when passed !integer' do
-    expect(fizzbuzz(!Integer)). to eq "Try again"
   end
 
 end
